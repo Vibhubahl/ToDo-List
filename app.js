03cdea8e9264ed60e2b3d906da9bca47291dde1e
Vaibhav Behl <vaibhav171865.cse@chitkara.edu.in>
2020-07-18 22:38:43 +0530

    add gitignore

--- app.js
+++ app.js
@@ -154,7 +154,13 @@ app.post("/del" , function(req,res)
     res.redirect("/");
 });*/
 
-app.listen(3000 ,function(req,res)
+let port = process.env.PORT;
+if(port==null || port=="")
+{
+	port=3000;
+}
+
+app.listen(port ,function(req,res)
 {
 	console.log("Started");
 })
